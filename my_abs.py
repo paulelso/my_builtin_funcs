ed3a2d1ba64bcc742480faab071b362600128665
P4ul0 <paulelso@hotmail.com>
2023-03-31 23:08:45 +0200

    Add files via upload

--- my_abs.py
+++ my_abs.py
@@ -1,8 +1,9 @@
-def my_pow(n, p):
-    if p == 0:
-        return 1
-    if n == 0:
-        return 0
-    
+def my_abs(n):
+    if n < 0:
+        return -n
+    else:
+        return n
+
 # run number in for loop to compare results
-print(my_pow(1, 0))
+for n in [-5, -3.14, 0, 9]:
+    print(abs(n), my_abs(n))
